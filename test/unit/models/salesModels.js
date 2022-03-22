28b7fcdf04fa73083e3bd76194efc4ed73607859
ZekaEu <zeka.surf@hotmail.com>
2022-03-22 12:27:49 -0300

    One more Try

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -1,87 +1,87 @@
-// const sinon = require('sinon');
-// const { expect } = require('chai');
-// const connection = require('../../../models/connection');
-// const Model = require('../../../models/salesModel');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../../models/connection');
+const Model = require('../../../models/salesModel');
 
-// describe('SALES MODEL', () => {
-//   const getAllResponse = [
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 1,
-//         "quantity": 5,
-//         "saleId": 1
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 2,
-//         "quantity": 10,
-//         "saleId": 1
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 3,
-//         "quantity": 15,
-//         "saleId": 2
-//     }
-//   ];
+describe('SALES MODEL', () => {
+  const getAllResponse = [
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 1,
+        "quantity": 5,
+        "saleId": 1
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 2,
+        "quantity": 10,
+        "saleId": 1
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 3,
+        "quantity": 15,
+        "saleId": 2
+    }
+  ];
 
-//   const getByIdResponse = [
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 1,
-//         "quantity": 5
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 2,
-//         "quantity": 10
-//     }
-//   ];
+  const getByIdResponse = [
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 1,
+        "quantity": 5
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 2,
+        "quantity": 10
+    }
+  ];
 
-//   describe('Função getAll', () => {
-//     before(() => {
-//       sinon.stub(connection, 'execute').resolves([getAllResponse, []]);
-//     });
+  describe('Função getAll', () => {
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([getAllResponse, []]);
+    });
 
-//     after(() => {
-//       connection.execute.restore();
-//     });
+    after(() => {
+      connection.execute.restore();
+    });
 
-//     it('Retorna um array de objetos', async () => {
-//       const sales = await Model.getAll();
-//       expect(sales).to.be.an('array');
-//       expect(sales).not.to.be.empty;
-//       sales.forEach((e) => expect(e).to.be.an('object'));
-//     });
-//   });
+    it('Retorna um array de objetos', async () => {
+      const sales = await Model.getAll();
+      expect(sales).to.be.an('array');
+      expect(sales).not.to.be.empty;
+      sales.forEach((e) => expect(e).to.be.an('object'));
+    });
+  });
 
-//   describe('Função getById', () => {
-//     before(() => {
-//       sinon.stub(connection, 'execute').resolves([[getByIdResponse], []]);
-//     });
+  describe('Função getById', () => {
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([[getByIdResponse], []]);
+    });
 
-//     after(() => {
-//       connection.execute.restore();
-//     });
+    after(() => {
+      connection.execute.restore();
+    });
 
-//     it('Retorna um array', async () => {
-//       const sale = await Model.getById();
-//       expect(sale).to.be.an('array');
-//     });
-//   });
+    it('Retorna um array', async () => {
+      const sale = await Model.getById();
+      expect(sale).to.be.an('array');
+    });
+  });
 
-//   describe('Função exclude', () => {
-//     before(() => {
-//       sinon.stub(connection, 'execute').resolves();
-//     });
+  describe('Função exclude', () => {
+    before(() => {
+      sinon.stub(connection, 'execute').resolves();
+    });
 
-//     after(() => {
-//       connection.execute.restore();
-//     })
+    after(() => {
+      connection.execute.restore();
+    })
 
-//     it('Chama connection.execute', async () => {
-//       await Model.exclude();
-//       expect(connection.execute.called).to.be.equal(true);
-//     });
-//   });
-// });
+    it('Chama connection.execute', async () => {
+      await Model.exclude();
+      expect(connection.execute.called).to.be.equal(true);
+    });
+  });
+});
--- test/unit/services/salesServices.js
+++ test/unit/services/salesServices.js
@@ -1,74 +1,74 @@
-// const sinon = require('sinon');
-// const { expect } = require('chai');
-// const Model = require('../../../models/salesModel');
-// const salesService = require('../../../services/salesService');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Model = require('../../../models/salesModel');
+const salesService = require('../../../services/salesService');
 
-// describe('SALES SERVICE', () => {
-//   const getAllResponse = [
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 1,
-//         "quantity": 5,
-//         "saleId": 1
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 2,
-//         "quantity": 10,
-//         "saleId": 1
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 3,
-//         "quantity": 15,
-//         "saleId": 2
-//     }
-//   ];
+describe('SALES SERVICE', () => {
+  const getAllResponse = [
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 1,
+        "quantity": 5,
+        "saleId": 1
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 2,
+        "quantity": 10,
+        "saleId": 1
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 3,
+        "quantity": 15,
+        "saleId": 2
+    }
+  ];
 
-//   const getByIdResponse = [
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 1,
-//         "quantity": 5
-//     },
-//     {
-//         "date": "2022-03-15T21:20:07.000Z",
-//         "productId": 2,
-//         "quantity": 10
-//     }
-//   ];
+  const getByIdResponse = [
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 1,
+        "quantity": 5
+    },
+    {
+        "date": "2022-03-15T21:20:07.000Z",
+        "productId": 2,
+        "quantity": 10
+    }
+  ];
 
-//   describe('Função getAll', () => {
-//     before(() => {
-//       sinon.stub(Model, 'getAll').resolves(getAllResponse);
-//     });
+  describe('Função getAll', () => {
+    before(() => {
+      sinon.stub(Model, 'getAll').resolves(getAllResponse);
+    });
 
-//     after(() => {
-//       Model.getAll.restore();
-//     });
+    after(() => {
+      Model.getAll.restore();
+    });
 
-//     it('Retorna um array de objetos', async () => {
-//       const sales = await salesService.getAll();
-//       expect(sales).to.be.an('array');
-//       expect(sales).to.not.be.empty;
-//       sales.forEach((e) => expect(e).to.be.an('object'));
-//     });
-//   });
+    it('Retorna um array de objetos', async () => {
+      const sales = await salesService.getAll();
+      expect(sales).to.be.an('array');
+      expect(sales).to.not.be.empty;
+      sales.forEach((e) => expect(e).to.be.an('object'));
+    });
+  });
 
-//   describe('Função getById', () => {
-//     before(() => {
-//       sinon.stub(Model, 'getById').resolves(getByIdResponse);
-//     });
+  describe('Função getById', () => {
+    before(() => {
+      sinon.stub(Model, 'getById').resolves(getByIdResponse);
+    });
 
-//     after(() => {
-//       Model.getById.restore();
-//     });
+    after(() => {
+      Model.getById.restore();
+    });
 
-//     it('Retorna um array de objetos', async () => {
-//       const sale = await salesService.getById();
-//       expect(sale).to.be.an('array');
-//       expect(sale).not.to.be.empty;
-//       sale.forEach((e) => expect(e).to.be.an('object'));
-//     });
-//   });
-// });
+    it('Retorna um array de objetos', async () => {
+      const sale = await salesService.getById();
+      expect(sale).to.be.an('array');
+      expect(sale).not.to.be.empty;
+      sale.forEach((e) => expect(e).to.be.an('object'));
+    });
+  });
+});
